80030a671ead50bffba63d849086b4cfed5254c4
hongdongsu <dsh@storecamera.io>
2020-02-08 21:08:04 +0900

    file Serve 등록

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -11,6 +11,12 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	// mux.Handle() 함수를 사용하여 파일을 /static/로 시작하는 모든 URL경로에 대한 핸들러로 등록
+	// 요청이 도착하기전에 /static 접두사를 제거한다
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
